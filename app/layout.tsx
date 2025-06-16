9ba7041c2221c811f87bec0177d02479dd11900c
vishvesh11 <vishusinghpal12@gmail.com>
2025-06-16 19:41:38 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,21 @@ import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
 import { cn } from '@/lib/utils';
 
-
-
 export const metadata: Metadata = {
   title: 'Software Engineer Portfolio',
   description:
     'Software Engineer, Self-Hosting Enthusiast, Building Scalable & Resilient Systems',
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' }, // For the primary favicon.ico
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png', // For apple-touch-icon
+  },
+  manifest: '/site.webmanifest', // For your web app manifest
 };
 
 export default function RootLayout({
